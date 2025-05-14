9c928808ba02635e5db42d1f896ddefbb959df16
Tamutswa Matondo <164413835+Tamutswa@users.noreply.github.com>
2025-05-15 00:02:19 +0200

    Update chart.tsx

--- components/ui/chart.tsx
+++ components/ui/chart.tsx
@@ -364,16 +364,5 @@ export {
   ChartStyle,
 }
 
-const isProd = process.env.NODE_ENV === 'production';
 
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  output: 'export',
-  assetPrefix: isProd ? '/My_Portfolio./' : '',
-  images: {
-    unoptimized: true,
-  },
-};
-
-export default nextConfig;
 
