c19b5ab983ec49b6dd4dab612affacc2915ff323
Tamutswa Matondo <164413835+Tamutswa@users.noreply.github.com>
2025-05-15 00:03:36 +0200

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -12,3 +12,16 @@ const nextConfig = {
 }
 
 export default nextConfig
+
+const isProd = process.env.NODE_ENV === 'production';
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: 'export',
+  assetPrefix: isProd ? '/My_Portfolio./' : '',
+  images: {
+    unoptimized: true,
+  },
+};
+
+export default nextConfig;
